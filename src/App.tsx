da6f2ae602847d0232290978ed572b740e84434a
Chanan Gosonsaksakun <craxidile@gmail.com>
2026-05-16 18:16:23 +0700

    fix: add scroll to top wrapper

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import {HashRouter, Routes, Route} from 'react-router-dom';
+import React, {PropsWithChildren, useEffect} from 'react';
+import {HashRouter, Route, Routes, useLocation} from 'react-router-dom';
 
 import ScreenFront from './components/screens/screen-front';
 import ScreenActivityList from './components/screens/screen-activity-list';
@@ -8,18 +8,31 @@ import ScreenAbout from "./components/screens/screen-about";
 import ScreenProject from "./components/screens/screen-project";
 import ScreenActivity from "./components/screens/screen-activity";
 
+const ScrollToTopWrapper = (props: PropsWithChildren) => {
+  const { children } = props;
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    document.documentElement.scrollTo(0, 0);
+  }, [pathname]);
+
+  return <>{children}</>;
+};
+
 function App() {
   return (
     <HashRouter>
-      <Routes>
-        <Route path="/" element={<ScreenFront />} />
-        <Route path="/activity" element={<ScreenActivityList />} />
-        <Route path="/activity/:id" element={<ScreenActivity />} />
-        <Route path="/project" element={<ScreenProjectList />} />
-        <Route path="/project/:id" element={<ScreenProject />} />
-        <Route path="/about" element={<ScreenAbout />} />
-        <Route path="/contact" element={<ScreenAbout />} />
-      </Routes>
+      <ScrollToTopWrapper>
+        <Routes>
+          <Route path="/" element={<ScreenFront/>}/>
+          <Route path="/activity" element={<ScreenActivityList/>}/>
+          <Route path="/activity/:id" element={<ScreenActivity/>}/>
+          <Route path="/project" element={<ScreenProjectList/>}/>
+          <Route path="/project/:id" element={<ScreenProject/>}/>
+          <Route path="/about" element={<ScreenAbout/>}/>
+          <Route path="/contact" element={<ScreenAbout/>}/>
+        </Routes>
+      </ScrollToTopWrapper>
     </HashRouter>
   );
 }
