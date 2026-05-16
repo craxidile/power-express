a93a48708a5ff93a4ccec5bcf79c15db9f5e3c0e
Chanan Gosonsaksakun <craxidile@gmail.com>
2026-05-16 18:20:57 +0700

    fix: add scroll to top wrapper

--- src/components/layouts/layout-banner/index.tsx
+++ src/components/layouts/layout-banner/index.tsx
@@ -1,6 +1,7 @@
-import {PropsWithChildren, ReactElement} from "react";
+import {PropsWithChildren, ReactElement, useEffect} from "react";
 import Nav, {NavTheme} from "../../_commons/nav";
 import Footer from "../../_commons/footer";
+import {IVmScreen, useVmScreen} from "../../../stores/vm-screen";
 
 export interface LayoutBannerProps {
   banner?: ReactElement;
@@ -9,6 +10,14 @@ export interface LayoutBannerProps {
 
 const LayoutBanner = (props: PropsWithChildren<LayoutBannerProps>) => {
   const { banner, isFilled, children } = props;
+
+  const vmScreen: IVmScreen = useVmScreen();
+
+  useEffect(() => {
+    if (!vmScreen.bind) return;
+    vmScreen.bind();
+  }, [vmScreen]);
+
   return (
     <div className="min-h-screen flex flex-col justify-start items-stretch">
       <div className={`relative h-screen lg:max-h-none lg:min-h-[900px] flex flex-col justify-stretch items-stretch ${!isFilled ? '' : 'max-h-[640px]'}`}>
--- src/components/layouts/layout-standard/index.tsx
+++ src/components/layouts/layout-standard/index.tsx
@@ -1,10 +1,18 @@
-import {PropsWithChildren} from "react";
+import {PropsWithChildren, useEffect} from "react";
 
 import Nav, {NavTheme} from "../../_commons/nav";
 import Footer from "../../_commons/footer";
+import {IVmScreen, useVmScreen} from "../../../stores/vm-screen";
 
 const LayoutStandard = (props: PropsWithChildren) => {
   const { children } = props;
+  const vmScreen: IVmScreen = useVmScreen();
+
+  useEffect(() => {
+    if (!vmScreen.bind) return;
+    vmScreen.bind();
+  }, [vmScreen]);
+
   return (
     <div className="min-h-screen flex flex-col justify-start items-stretch">
       <div className="h-[112px] flex flex-row justify-start items-stretch">
