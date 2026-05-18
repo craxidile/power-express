6f9174c8c3beb299e166f88079eea3068c8498f8
Chanan Gosonsaksakun <craxidile@gmail.com>
2026-05-18 11:05:42 +0700

    chore: add image preview

--- src/components/_commons/photo-gallery/index.tsx
+++ src/components/_commons/photo-gallery/index.tsx
@@ -1,4 +1,5 @@
-import {PropsWithChildren, useCallback, useMemo} from "react";
+import {PropsWithChildren, useCallback, useMemo, useState} from "react";
+import {PhotoSlider} from "react-photo-view";
 
 const MAX_IMAGES_WITHOUT_HIDDEN = 4;
 
@@ -9,33 +10,43 @@ export interface PhotoGalleryProps {
 const PhotoGallery = (props: PropsWithChildren<PhotoGalleryProps>) => {
   const {photos} = props;
 
+  const [previewIndex, setPreviewIndex] = useState(0);
+  const [previewVisible, setPreviewVisible] = useState(false);
+
   const photoCount = useMemo(() => photos.length, [photos]);
   const lastIndex = useMemo(() => MAX_IMAGES_WITHOUT_HIDDEN - 1, []);
   const moreCount = useMemo(() => photoCount - MAX_IMAGES_WITHOUT_HIDDEN, [photoCount]);
   const gridPhotos = useMemo(() => photos.slice(0, MAX_IMAGES_WITHOUT_HIDDEN), [photos]);
 
   const onClick = useCallback((index: number) => {
-    //
+    setPreviewIndex(index);
+    setPreviewVisible(true);
   }, []);
 
   return (
-    <div className="gap-4 grid grid-cols-1 lg:grid-cols-2">
-      {gridPhotos.map((image, index) => {
-        return (
-          <button key={`${image}-${index}`} onClick={() => onClick(index)}
-                  className="relative block aspect-[519/358] bg-gray-pale"
-                  style={{background: `url(${image}) no-repeat center/cover`}}>
-            {photoCount <= MAX_IMAGES_WITHOUT_HIDDEN || index < lastIndex ? null : (
-              <div className="absolute left-0 top-0 w-full h-full bg-black-a50 gap-y-4 flex flex-col justify-center items-center">
-                <span className="text-white text-base">See more</span>
-                <span className="text-6hxl text-cta-primary">+{moreCount}</span>
-                <span className="text-white text-base">Photos</span>
-              </div>
-            )}
-          </button>
-        );
-      })}
-    </div>
+    <>
+      <div className="gap-4 grid grid-cols-1 lg:grid-cols-2">
+        {gridPhotos.map((image, index) => {
+          return (
+            <button key={`${image}-${index}`} onClick={() => onClick(index)}
+                    className="relative block aspect-[519/358] bg-gray-pale"
+                    style={{background: `url(${image}) no-repeat center/cover`}}>
+              {photoCount <= MAX_IMAGES_WITHOUT_HIDDEN || index < lastIndex ? null : (
+                <div
+                  className="absolute left-0 top-0 w-full h-full bg-black-a50 gap-y-4 flex flex-col justify-center items-center">
+                  <span className="text-white text-base">See more</span>
+                  <span className="text-6hxl text-cta-primary">+{moreCount}</span>
+                  <span className="text-white text-base">Photos</span>
+                </div>
+              )}
+            </button>
+          );
+        })}
+      </div>
+      <PhotoSlider visible={previewVisible} index={previewIndex} onIndexChange={setPreviewIndex}
+                   onClose={() => setPreviewVisible(false)}
+                   images={photos.map((p) => ({ src: p, key: p }))} />
+    </>
   );
 };
 
