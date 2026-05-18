96d8f0f164647a80065c47dbd85124e7c34bcd54
Chanan Gosonsaksakun <craxidile@gmail.com>
2026-05-18 10:18:15 +0700

    chore: add mobile nav menu

--- src/components/layouts/layout-popup/index.tsx
+++ src/components/layouts/layout-popup/index.tsx
@@ -26,7 +26,7 @@ const LayoutPopup = (props: PropsWithChildren<LayoutPopupProps>) => {
   // }, [visible]);
 
   const pointerEvents = useMemo(() => {
-    return !visible ? 'pointer-events-none translate-x-[100vw] delay-300 ease-out overflow-y-hidden' : 'translate-x-0 delay-200 ease-in';
+    return !visible ? 'pointer-events-none translate-x-[100vw] delay-300 ease-out' : 'translate-x-0 delay-200 ease-in overflow-y-hidden';
   }, [visible]);
 
   return (
