d09b1be60782db3599d0b691f1af2cc1bed700cb
Chanan Gosonsaksakun <craxidile@gmail.com>
2026-05-29 23:48:48 +0700

    chore: add benefit, hero banner, partner api clients

--- src/apis/benefit/index.ts
+++ src/apis/benefit/index.ts
@@ -1,6 +1,17 @@
+import axios from 'axios';
+
 import { Benefit } from '../../models/benefit';
-import mockBenefits from './mock/benefits';
+import { apiKey, apiPrefix } from '../../config/constants';
+// import mockBenefits from './mock/benefits';
 
 export const listBenefits = async (): Promise<Benefit[]> => {
-  return Promise.resolve(mockBenefits);
+  // return Promise.resolve(mockBenefits);
+  const response = await axios.get<Benefit[]>(
+    `${apiPrefix}/list-all-benefits`,
+    {
+      headers: { apiKey },
+    }
+  );
+  const { data } = response;
+  return data;
 };
--- src/apis/partner/index.ts
+++ src/apis/partner/index.ts
@@ -1,6 +1,17 @@
+import axios from 'axios';
+
 import { Partner } from '../../models/partner';
-import mockPartners from './mock/partners';
+import { apiKey, apiPrefix } from '../../config/constants';
+// import mockPartners from './mock/partners';
 
 export const listPartners = async (): Promise<Partner[]> => {
-  return Promise.resolve(mockPartners);
+  // return Promise.resolve(mockPartners);
+  const response = await axios.get<Partner[]>(
+    `${apiPrefix}/list-all-partners`,
+    {
+      headers: { apiKey },
+    }
+  );
+  const { data } = response;
+  return data;
 };
