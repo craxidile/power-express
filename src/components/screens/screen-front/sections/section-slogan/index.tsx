09fa8017a77f3d754bf6358254b3447a8704549f
Chanan Gosonsaksakun <craxidile@gmail.com>
2026-05-29 22:23:09 +0700

    fix: fix slogan section

--- src/components/screens/screen-front/sections/section-slogan/index.tsx
+++ src/components/screens/screen-front/sections/section-slogan/index.tsx
@@ -10,14 +10,14 @@ const SectionSlogan = () => {
 
   return (
     <section
-      className="relative h-screen lg:min-h-[900px] bg-gray-100 flex flex-col justify-start items-stretch"
+      className="relative h-screen lg:min-h-[720px] bg-gray-100 flex flex-col justify-start items-stretch"
       style={{
         background: `url(${p('mock/front/section-slogan/bg.jpg')}) no-repeat center/cover`,
       }}
     >
       <div className="absolute left-0 top-0 w-full h-full bg-black-a30 backdrop-blur-xs" />
       <Nav />
-      <div className="px-[52px] lg:px-0 relative z-[1] pt-24 lg:pt-[120px] flex-1 flex flex-col justify-center items-center">
+      <div className="px-[52px] relative z-[1] lg:px-0 flex-1 flex flex-col justify-center items-center">
         <div className="flex flex-col justify-start items-stretch">
           <p className="hidden lg:block text-4xl lg:text-6xl text-white lg:text-center !leading-[1.2]">
             <TextLines text={l(locale, localizations, 'front.cta')} />
