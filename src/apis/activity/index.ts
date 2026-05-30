58f332e2a30c8aac64783b85afaa799ef3275f7b
Chanan Gosonsaksakun <craxidile@gmail.com>
2026-05-30 22:55:06 +0700

    fix: change mock activity api request to server api request

--- src/apis/activity/index.ts
+++ src/apis/activity/index.ts
@@ -1,16 +1,43 @@
+import axios from 'axios';
+
 import { Activity } from '../../models/activity';
-import mockActivities from './mock/activities';
+import { apiKey, apiPrefix } from '../../config/constants';
+// import mockActivities from './mock/activities';
 
 export const listLatestActivities = async (): Promise<Activity[]> => {
-  return Promise.resolve(mockActivities.slice(0, 3));
+  // return Promise.resolve(mockActivities.slice(0, 3));
+  const response = await axios.get<Activity[]>(
+    `${apiPrefix}/list-latest-activities`,
+    {
+      headers: { apiKey },
+    }
+  );
+  const { data } = response;
+  return data;
 };
 
 export const listActivities = async (): Promise<Activity[]> => {
-  return Promise.resolve(mockActivities);
+  // return Promise.resolve(mockActivities);
+  const response = await axios.get<Activity[]>(
+    `${apiPrefix}/list-all-activities`,
+    {
+      headers: { apiKey },
+    }
+  );
+  const { data } = response;
+  return data;
 };
 
 export const findActivityById = async (
   id: number
 ): Promise<Activity | null> => {
-  return Promise.resolve(mockActivities.find((p) => p.id === id) ?? null);
+  // return Promise.resolve(mockActivities.find((p) => p.id === id) ?? null);
+  const response = await axios.get<Activity | null>(
+    `${apiPrefix}/find-activity-by-id?id=${id}`,
+    {
+      headers: { apiKey },
+    }
+  );
+  const { data } = response;
+  return data;
 };
